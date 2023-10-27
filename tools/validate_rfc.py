d8b67dd1df10feb0774f7f3c998edee4e4d64505
SamG-UAG <samuel.gutierrez@edu.uag.mx>
2023-10-26 23:22:04 -0600

    Add validate RFC

--- tools/validate_rfc.py
+++ tools/validate_rfc.py
@@ -1,22 +1,23 @@
 import re
 
-with open("rfc.txt", "r") as file:
-    data = file.read()
+def get_RFC_validation():
+    with open("rfc.txt", "r") as file:
+        data = file.read()
 
-pattern = r'\b([A-Z]{4}\d{6}[A-Z0-9]{3})\b|(?<=RFC\s)([nv])'
+    pattern = r'\b([A-Z]{4}\d{6}[A-Z0-9]{3})\b|(?<=RFC\s)([nv])'
 
-matches = re.finditer(pattern, data)
+    matches = re.finditer(pattern, data)
 
-prev_rfc = None
+    prev_rfc = None
 
-result = {}
+    result = {}
 
-for match in matches:
-    rfc, status = match.groups()
-    
-    if rfc:
-        prev_rfc = rfc
-    elif status == 'n' and prev_rfc:
-        result[prev_rfc] = 'n'
-    elif status == 'v' and prev_rfc:
-        result[prev_rfc] = 'v'
+    for match in matches:
+        rfc, status = match.groups()
+        
+        if rfc:
+            prev_rfc = rfc
+        elif status == 'n' and prev_rfc:
+            result[prev_rfc] = 'n'
+        elif status == 'v' and prev_rfc:
+            result[prev_rfc] = 'v'
